5c6f4860840522a1279df10db0b1b71dc79d0101
Fernando de la Rosa <fernando.delarosa@alegra.com>
2023-09-07 21:22:47 -0600

    Add form components and sub components

--- src/lib/components/ui/avatar/avatar-fallback.svelte
+++ src/lib/components/ui/avatar/avatar-fallback.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import { Avatar as AvatarPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
+	import { Avatar as AvatarPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
 
 	type $$Props = AvatarPrimitive.FallbackProps;
 
-	let className: $$Props["class"] = undefined;
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
 <AvatarPrimitive.Fallback
 	class={cn(
-		"flex h-full w-full items-center justify-center rounded-full bg-muted",
+		'flex h-full w-full items-center justify-center text-sm rounded-full bg-muted',
 		className
 	)}
 	{...$$restProps}
